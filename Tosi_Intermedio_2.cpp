ddb82a54ff91630dde5b1d153ffc69da60c3090f
Gianmarco <gmarco.tosi@gmail.com>
2026-01-18 16:07:07 +0100

    commit

---
+++ Tosi_Intermedio_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void passoavanti(int v[], int n) {
+
+
+    for (int i = n - 1; i > 0; i--) {
+        v[i] = v[i - 1];
+    }
+
+    v[0] = 0;
+}
+int main (){
+
+    int dim;
+    cout << "quanto e' grande l'array?" << endl;
+    cin >> dim;
+    int v[dim];
+    for (int i = 0; i < dim; i++) {
+        cout << "inserisci i valori nell'array" << endl;
+        cin >> v[i];
+    }
+
+    passoavanti(v, dim);
+    for (int i = 0; i < dim; i++) {
+    cout << v[i] << endl;}
+
+}
